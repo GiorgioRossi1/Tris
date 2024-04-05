d420723e9f2f5d02c37ed7c7cab6fd81e59bad61
GiorgioRossi1 <giorgio.rossi@iisgubbio.edu.it>
2024-04-05 10:16:03 +0200

    logo spostato

--- src/it/edu/iisgubbio/giocoFinale/InterfacciaGioco.java
+++ src/it/edu/iisgubbio/giocoFinale/InterfacciaGioco.java
@@ -1,42 +1,26 @@
 package it.edu.iisgubbio.giocoFinale;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
-public class InterfacciaGioco extends Application {
-
-    private Button[][] buttons = new Button[3][3];
-    private boolean isPlayerX = true;
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) {
-        GridPane gridPane = new GridPane();
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                Button button = new Button();
-                button.setMinSize(100, 100);
-                button.setOnAction(e -> {
-                    if (button.getText().isEmpty()) {
-                        button.setText(isPlayerX ? "X" : "O");
-                        isPlayerX = !isPlayerX;
-                    }
-                });
-                buttons[i][j] = button;
-                gridPane.add(button, i, j);
-            }
-        }
-
-        Scene scene = new Scene(gridPane, 300, 300);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Tris Game");
-        primaryStage.show();
-    }
+public class InterfacciaGioco extends Application{
+		
+		GridPane griglia = new GridPane();
+		Label lTitolo = new Label("TRIS");
+		Label lNomi = new Label("Rossi Giorgio, Luchetti Simone, Menichetti Lorenzo");
+		Button bGioca = new Button("Gioca");
+		
+		public void start(Stage finestra) {
+			griglia.add(lTitolo, 0, 0)
+			;
+		}
+	
+	public static void main(String args[]){
+		launch();
+	}
 }
