ac637d6d915f363a1d51e3004f68a650529b2ee0
Pallinojr <lorenzo.menichetti07@iisgubbio.edu.it>
2024-04-17 09:41:41 +0200

    interfaccia quasi completa

--- src/it/edu/iisgubbio/giocoFinale/InterfacciaGioco.java
+++ src/it/edu/iisgubbio/giocoFinale/InterfacciaGioco.java
@@ -3,12 +3,15 @@ package it.edu.iisgubbio.giocoFinale;
 import javafx.application.Application;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
+import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.RowConstraints;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
@@ -16,9 +19,10 @@ import javafx.stage.Stage;
 public class InterfacciaGioco extends Application{
 		
 		GridPane griglia = new GridPane();
+		Pane grigliaInserita = new Pane();
 		Label lTitolo = new Label("TRIS");
-		Label lGiocatoreSingolo = new Label("giocatore singolo");
-		Label lDueGiocatori = new Label("multiplayer");
+		Label lGiocatoreSingolo = new Label("VS computer");
+		Label lDueGiocatori = new Label("VS altro giocatore");
 		Label lNomi = new Label("Rossi Giorgio, Luchetti Simone, Menichetti Lorenzo");
 		Button bGioca = new Button("Gioca");
 		Button bGiocatoreSingolo = new Button("seleziona");
@@ -35,6 +39,7 @@ public class InterfacciaGioco extends Application{
 			griglia.add(lNomi, 0, 1);
 			griglia.add(bGioca, 0, 3);
 			griglia.add(iw, 0, 2);
+			
 		
 			
 			lTitolo.getStyleClass().add("tris");
@@ -45,7 +50,42 @@ public class InterfacciaGioco extends Application{
 			bGioca.setMaxWidth(200);
 			ColumnConstraints Prima = new ColumnConstraints();
 			Prima.setHalignment(HPos.CENTER);
+			ColumnConstraints Seconda = new ColumnConstraints();
+			Seconda.setHalignment(HPos.CENTER);
+			ColumnConstraints Terza = new ColumnConstraints();
+			Terza.setHalignment(HPos.CENTER);
+			ColumnConstraints Quarta = new ColumnConstraints();
+			Quarta.setHalignment(HPos.CENTER);
+			ColumnConstraints Quinta = new ColumnConstraints();
+			Quinta.setHalignment(HPos.CENTER);
+			ColumnConstraints Sesta = new ColumnConstraints();
+			Sesta.setHalignment(HPos.CENTER);
+			RowConstraints vincoliRiga0 = new RowConstraints();
+			vincoliRiga0.setValignment(VPos.CENTER);
+			RowConstraints vincoliRiga1 = new RowConstraints();
+			vincoliRiga1.setValignment(VPos.CENTER);
+			RowConstraints vincoliRiga2 = new RowConstraints();
+			vincoliRiga2.setValignment(VPos.CENTER);
+			RowConstraints vincoliRiga3 = new RowConstraints();
+			vincoliRiga3.setValignment(VPos.CENTER);
+			RowConstraints vincoliRiga4 = new RowConstraints();
+			vincoliRiga4.setValignment(VPos.CENTER);
+			RowConstraints vincoliRiga5 = new RowConstraints();
+			vincoliRiga5.setValignment(VPos.CENTER);
 			griglia.getColumnConstraints().addAll(Prima);
+			griglia.getColumnConstraints().addAll(Seconda);
+			griglia.getColumnConstraints().addAll(Terza);
+			griglia.getColumnConstraints().addAll(Quarta);
+			griglia.getColumnConstraints().addAll(Quinta);
+			griglia.getColumnConstraints().addAll(Sesta);
+			griglia.getRowConstraints().addAll(vincoliRiga0);
+			griglia.getRowConstraints().addAll(vincoliRiga1);
+			griglia.getRowConstraints().addAll(vincoliRiga2);		
+			griglia.getRowConstraints().addAll(vincoliRiga3);			
+			griglia.getRowConstraints().addAll(vincoliRiga4);		
+			griglia.getRowConstraints().addAll(vincoliRiga5);
+			
+			
 			bGioca.setMaxWidth(300);
 			
 			bGiocatoreSingolo.setOnAction(e ->singolo());
@@ -54,8 +94,10 @@ public class InterfacciaGioco extends Application{
 			
 			
 			griglia.setPadding(new Insets(5, 5, 5, 5));
-			griglia.setHgap(15); 
-			griglia.setVgap(15);
+			griglia.setHgap(30); 
+			griglia.setVgap(30);
+			griglia.setPrefHeight(970);
+			griglia.setPrefWidth(300);
 			Scene scena = new Scene(griglia);
 			finestra.setTitle("tris");
 			finestra.setScene(scena);
@@ -63,33 +105,41 @@ public class InterfacciaGioco extends Application{
 			scena.getStylesheets().add("it/edu/iisgubbio/giocoFinale/Stile.css");
 			}
 		public void gioca() {
-			lTitolo.setVisible(false);
-			lNomi.setVisible(false);
-			bGioca.setVisible(false);
-			iw.setVisible(false);
-			griglia.add(lGiocatoreSingolo, 0, 0);
-			griglia.add(lDueGiocatori, 1, 0);
-			griglia.add(bGiocatoreSingolo, 0, 2);
-			griglia.add(bDueGiocatori, 1, 2);
+//			lTitolo.setVisible(false);
+//			lNomi.setVisible(false);
+//			bGioca.setVisible(false);
+//			iw.setVisible(false);
+			griglia.getChildren().remove(lTitolo);
+			griglia.getChildren().remove(lNomi);
+			griglia.getChildren().remove(bGioca);
+			griglia.getChildren().remove(iw);
+			//griglia.setGridLinesVisible(true);;
+			griglia.add(lGiocatoreSingolo, 3, 0);
+			griglia.add(lDueGiocatori, 3, 3);
+			griglia.add(bGiocatoreSingolo, 2, 1);
+			griglia.add(bDueGiocatori, 2, 4);
 			Image i = new Image(getClass().getResourceAsStream("single.png"));
 		    ImageView si = new ImageView(i);
 		    BorderPane nome1 = new BorderPane(si);
 		    Image i2 = new Image(getClass().getResourceAsStream("multi.png"));
 		    ImageView mu = new ImageView(i2);
 		    BorderPane nome2 = new BorderPane(mu);
-		    griglia.add(nome1, 0,1);
-		    griglia.add(nome2, 1, 1);
-		    lGiocatoreSingolo.getStyleClass().add("label2");
+		    BorderPane nome3 = new BorderPane(iw);
+		    griglia.add(nome1, 3,1);
+		    griglia.add(nome2, 3, 4);
+		    griglia.add(nome3, 3, 6);
+		    griglia.add(grigliaInserita,0,3);
 			lDueGiocatori.getStyleClass().add("label2");
+			lGiocatoreSingolo.getStyleClass().add("label2");
 			bDueGiocatori.getStyleClass().add("bottone");
 			bGiocatoreSingolo.getStyleClass().add("bottone");
 			si.setFitWidth(200);
 		    mu.setFitHeight(200);
+		    iw.setFitHeight(300);
+		    iw.setFitWidth(300);
 			nome1.getStyleClass().add("img");
 			nome2.getStyleClass().add("img");
-		
-		
-		
+
 		}
 		public void singolo() {
 			
